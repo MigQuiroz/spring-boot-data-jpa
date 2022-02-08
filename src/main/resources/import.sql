07ce4c71f920082ed7b9c85e57d52cc9730fe991
Miguel Quiroz <MQuiroz@acsicorp.com>
2022-02-08 12:01:54 -0600

    primer commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzman', 'andreguz@bolsaideas.com','2022-02-08');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2,'Miguel', 'Quiroz','miguel@mail.com','2022-02-08');
