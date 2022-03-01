349ec1c1cb76103bd23308cc0eb73321145e2a4c
Miguel Quiroz <MQuiroz@acsicorp.com>
2022-02-28 19:57:22 -0600

    Form Totalplay

--- src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -7,9 +7,9 @@ import com.bolsadeideas.springboot.app.models.entity.Cliente;
 public interface IClienteDao {
 
 	public List<Cliente> findAll();
-	
+	//guardar cliente
 	public void save(Cliente cliente);
-	
+	//para buscar cliente por id
 	public Cliente findOne(Long id);
 }
  
